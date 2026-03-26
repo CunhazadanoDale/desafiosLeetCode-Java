03067f48840e1accb37bbf43b42a33296cb43195
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-25 21:23:46 -0300

    feat: desafio do calculo simples com logica amplificada para melhor absorção do codigo

--- src/Desafio5.java
+++ src/Desafio5.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Desafio5 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int codigo1 = sc.nextInt();
+        int numero1 = sc.nextInt();
+        double unitValor1 = sc.nextDouble();
+
+        int codigo2 = sc.nextInt();
+        int numero2 = sc.nextInt();
+        double unitValor2 = sc.nextDouble();
+
+        double calculo1 = numero1 * unitValor1;
+        double calculo2 = numero2 * unitValor2;
+
+        double resultado = calculo1 + calculo2;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", resultado);
+
+    }
+
 }
