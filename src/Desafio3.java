3cd6562fc353d426e2491538dd88450ca5535932
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-25 21:22:57 -0300

    feat: desafio de calcular a area do triangulo

--- src/Desafio3.java
+++ src/Desafio3.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Desafio3 {
+
+    public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+
+       double raio = sc.nextDouble();
+       double pi = 3.14159;
+
+       double resultado = pi * (raio * raio);
+
+        System.out.printf("A=%.4f\n", resultado);
+    }
+
 }
