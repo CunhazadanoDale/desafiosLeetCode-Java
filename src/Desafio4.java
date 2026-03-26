640785b2bdd2d62a51efd0de71b3743515142829
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-25 21:23:22 -0300

    feat: desafio de calcular salario de um funcionario

--- src/Desafio4.java
+++ src/Desafio4.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Desafio4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numeroFuncionario = sc.nextInt();
+        int horaTrabalhada = sc.nextInt();
+        double ganhaHora = sc.nextDouble();
+
+        double salario = ganhaHora * horaTrabalhada;
+
+
+        System.out.println("NUMBER = " + numeroFuncionario);
+        System.out.printf("SALARY = U$ %.2f\n", salario);
+    }
+
 }
