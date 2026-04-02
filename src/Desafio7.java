1f4230378c6cf87c0df95441b6356cd90c44f1cf
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-01 21:21:05 -0300

    feat: desafio do destrinchar de notas

--- src/Desafio7.java
+++ src/Desafio7.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio7 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int valor = sc.nextInt();
+
+        int[] notas = {100, 50, 20, 10, 5, 2, 1};
+
+        System.out.println(valor);
+
+        for (int nota : notas) {
+            int quantidade = valor / nota;
+            valor = valor % nota;
+
+            System.out.println(quantidade + " nota(s) de R$ " + nota + ",00");
+        }
+
+    }
 }
