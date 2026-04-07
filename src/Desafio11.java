25392856d2b383f8b037e5ecadea54762838b547
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-06 22:29:59 -0300

    feat: desafio dos triangulos ou retangulos

--- src/Desafio11.java
+++ src/Desafio11.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Desafio11 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if ( a % b == 0 || b % a == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+    }
 }
