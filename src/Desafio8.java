1b75c7080fc346911ec366aef5e4b1b249dbd0f1
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-06 22:30:22 -0300

    feat: desafio da media ponderada 3

--- src/Desafio8.java
+++ src/Desafio8.java
@@ -6,19 +6,36 @@ public class Desafio8 {
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        float d = sc.nextFloat();
 
-        boolean valoresAceitos =
-                (b > c) && (d > a) && (c + d > a + b) && (c > 0) && (d > 0) && (a % 2 == 0);
+        int[] pesos = {2, 3, 4, 1};
 
-        if (valoresAceitos) {
-            System.out.println("Valores aceitos");
+        float media = ( (a * pesos[0]) + (b * pesos[1]) + (c * pesos[2]) + (d * pesos[3]) ) / (pesos[0] + pesos[1] + pesos[2] + pesos[3]);
+
+        System.out.printf("Media: %.1f\n", media);
+        if(media >= 7.0) {
+            System.out.println("Aluno aprovado.");
+        } else if (media < 5.0) {
+            System.out.println("Aluno reprovado.");
         } else {
-            System.out.println("Valores nao aceitos");
+            System.out.println("Aluno em exame.");
+            float e = sc.nextFloat();
+            System.out.println("Nota do exame: " + e);
+
+            float resultado = ( media + e ) / 2;
+
+            if (resultado >= 5) {
+                System.out.println("Aluno aprovado.");
+            } else {
+                System.out.println("Aluno reprovado.");
+            }
+
+            System.out.println("Media final: " + resultado);
         }
+
 
     }
 }
