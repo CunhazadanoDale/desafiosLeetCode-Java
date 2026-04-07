52101d3a60233b1038881c65de0e42cb37e219e0
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-06 22:30:44 -0300

    feat: desafio da ordenação de array

--- src/Desafio9.java
+++ src/Desafio9.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Desafio9 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] leitura = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        int[] copia = leitura.clone();
+
+        Arrays.sort(copia);
+
+        for (int num : copia) {
+            System.out.println(num);
+        }
+
+        System.out.println();
+
+        for (int num : leitura) {
+            System.out.println(num);
+        }
+
+    }
 }
