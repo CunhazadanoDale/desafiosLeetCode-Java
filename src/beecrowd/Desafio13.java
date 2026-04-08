a810de52366ec6c26d6db0cbbdbf54099d03b2a9
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-07 22:31:25 -0300

    feat: media dos ponteiros utilizando contador

--- src/beecrowd/Desafio13.java
+++ src/beecrowd/Desafio13.java
@@ -1,4 +1,29 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Desafio13 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        float[] entrada = {sc.nextFloat(), sc.nextFloat(), sc.nextFloat(),
+                sc.nextFloat(), sc.nextFloat(), sc.nextFloat()};
+
+        int contador = 0;
+        float soma = 0;
+
+        for (float numero : entrada) {
+            if (numero > 0) {
+                contador++;
+                soma += numero;
+            }
+        }
+
+        double media = soma / contador;
+
+        System.out.println(contador + " valores positivos");
+        System.out.printf("%.1f\n", media);
+
+    }
 }
