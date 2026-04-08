c6f512312821bf6276ce7e372d61039850d9570d
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-07 22:31:47 -0300

    feat: contador numeros impares

--- src/beecrowd/Desafio14.java
+++ src/beecrowd/Desafio14.java
@@ -1,4 +1,21 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Desafio14 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+       int inteiro = sc.nextInt();
+       int contador = 0;
+
+       for (int i = inteiro; i < 100; i++ ) {
+           if (i % 2 != 0 && contador < 6) {
+               System.out.println(i);
+               contador++;
+           }
+       }
+
+    }
 }
