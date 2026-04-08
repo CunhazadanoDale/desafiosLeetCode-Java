ae21264ef86a141e608872ffb18a495ffe385a72
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-07 22:39:57 -0300

    feat: adicionado codigo para soma de numeros impares entre duas variaveis

--- src/beecrowd/Desafio15.java
+++ src/beecrowd/Desafio15.java
@@ -1,4 +1,30 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Desafio15 {
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero1 = sc.nextInt();
+        int numero2 = sc.nextInt();
+        int soma = 0;
+
+        if (numero2 > numero1) {
+            for (int i = numero1; i < numero2; i++) {
+                if(i % 2 != 0 && i != numero1) {
+                    soma += i;
+                }
+            }
+        } else {
+            for (int i = numero1; i > numero2; i--) {
+                if (i % 2 != 0 && i != numero1) {
+                    soma += i;
+                }
+            }
+        }
+
+        System.out.println(soma);
+    }
 }
