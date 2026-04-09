57d704def7f46facdd2ce164a5eece07ccb2e651
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-08 22:51:14 -0300

    feat: divisão de tipos através das entradas pares ou impares

--- src/beecrowd/Desafio16.java
+++ src/beecrowd/Desafio16.java
@@ -1,4 +1,27 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Desafio16 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero1 = sc.nextInt();
+        int contadorDentro = 0;
+        int contadorFora = 0;
+
+        for (int i = 0; i < numero1; i++) {
+            int numero2 = sc.nextInt();
+
+            if(numero2 >= 10 && numero2 <= 20) {
+                contadorDentro++;
+            } else {
+                contadorFora++;
+            }
+        }
+
+        System.out.println(contadorDentro + " in");
+        System.out.println(contadorFora + " out");
+    }
 }
