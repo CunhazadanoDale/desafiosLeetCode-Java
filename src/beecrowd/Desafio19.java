9d7ec0b9c500c98e17fe9e5c0e90ed93f4b5d92d
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-08 22:52:36 -0300

    feat: mostrando todos numeros de 1 a 10.000 que sao divididos por um numero e restam 2

--- src/beecrowd/Desafio19.java
+++ src/beecrowd/Desafio19.java
@@ -1,4 +1,18 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Desafio19 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero1 = sc.nextInt();
+
+        for (int i = 1; i <= 10000; i++) {
+            if (i % numero1 == 2) {
+                System.out.println(i);
+            }
+        }
+    }
 }
