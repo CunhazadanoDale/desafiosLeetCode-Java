45dbafcd2c1db3fa7788830833deb5c36f8f8d59
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-08 22:52:11 -0300

    feat: informando numeros pares ou impares atraves de metodo externo

--- src/beecrowd/Desafio18.java
+++ src/beecrowd/Desafio18.java
@@ -1,4 +1,36 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Desafio18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero1 = sc.nextInt();
+
+        for (int i = 1; i <= numero1; i++) {
+
+            int numero2 = sc.nextInt();
+
+            if(numero2 == 0) {
+                System.out.println(checaPositivoNegativo(numero2));
+            } else if (numero2 % 2 != 0) {
+                System.out.println("ODD " + checaPositivoNegativo(numero2));
+            } else {
+                System.out.println("EVEN " + checaPositivoNegativo(numero2));
+            }
+
+        }
+    }
+
+    public static String checaPositivoNegativo(int num) {
+        if (num > 0) {
+            return "POSITIVE";
+        } else if (num < 0) {
+            return "NEGATIVE";
+        } else {
+            return "NULL";
+        }
+    }
 }
