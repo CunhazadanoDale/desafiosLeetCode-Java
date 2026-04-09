21b3541731e5acc45e6daa64b1c751343539df9d
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-08 22:50:50 -0300

    feat: codigo de separação dos numeros atraves de suas somas

--- src/beecrowd/Desafio15.java
+++ src/beecrowd/Desafio15.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Desafio15 {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int numero1 = sc.nextInt();
