09dbe3b9232115d6106d5f462f42224d454f7c2f
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-08 22:51:39 -0300

    feat: mostrando a potencia dos numeros

--- src/beecrowd/Desafio17.java
+++ src/beecrowd/Desafio17.java
@@ -1,4 +1,18 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Desafio17 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero1 = sc.nextInt();
+
+        for (int i = 1; i <= numero1; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i + "^2 = " + i * i);
+            }
+        }
+    }
 }
