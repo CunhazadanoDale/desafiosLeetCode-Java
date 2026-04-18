61dab47f60e83d68ef814d8185c8d77d1ee2335c
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-17 22:55:27 -0300

    feat: desafio vetores

--- src/Desafio30.java
+++ src/Desafio30.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio30 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        float[] inteiros = new float[100];
+
+        for (int i = 0; i < inteiros.length; i++) {
+
+            inteiros[i] = scanner.nextFloat();
+
+            if (inteiros[i] <= 10) {
+                System.out.printf("A[%d] = %.1f\n", i, inteiros[i]);
+            }
+
+        }
+
+    }
 }
