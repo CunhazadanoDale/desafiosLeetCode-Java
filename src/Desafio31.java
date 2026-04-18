181c95324b928c706be5adbbd8f11f73ea61b817
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-18 09:13:09 -0300

    fix: correção do problema enfrentado anteriormente

--- src/Desafio31.java
+++ src/Desafio31.java
@@ -4,21 +4,21 @@ public class Desafio31 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] inteiros = new int[6];
+        int[] inteiros = new int[20];
 
         for (int i = 0; i < inteiros.length; i++) {
 
             inteiros[i] = scanner.nextInt();
         }
 
+        for (int i = 0; i < inteiros.length / 2; i++) {
+
+            int temp = inteiros[i];
+            inteiros[i] = inteiros[inteiros.length - 1 - i];
+            inteiros[inteiros.length - 1 - i] = temp;
+        }
+
         for (int i = 0; i < inteiros.length; i++) {
-            int numeroInicial = inteiros[i];
-            int numeroFInal = inteiros[inteiros.length - 1];
-
-            inteiros[i] = numeroFInal;
-            inteiros[inteiros.length - 1] = numeroInicial;
-
-
             System.out.println("N["+i+"] = " + inteiros[i]);
         }
 
