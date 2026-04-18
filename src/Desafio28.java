923ace9e3593662eb36397c39a74caf17cf5d825
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-17 22:55:00 -0300

    fix: desafio vetor

--- src/Desafio28.java
+++ src/Desafio28.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio28 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] ints = new int[10];
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = scanner.nextInt();
+
+            if (ints[i] > 0) {
+                System.out.println("X[" + i + "] = " + ints[i]);
+            } else {
+                ints[i] = 1;
+                System.out.println("X[" + i + "] = " + 1);
+            }
+        }
+    }
 }
