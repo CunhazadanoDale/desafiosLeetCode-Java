f6fb5479b8106e4cb0a897592f68c63e7b2d1c12
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-17 22:55:15 -0300

    feat: desafio de vetores

--- src/Desafio29.java
+++ src/Desafio29.java
@@ -1,7 +1,25 @@
-package beecrowd;
+import java.util.Scanner;
 
 public class Desafio29 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int[] ints = new int[10];
+        int somador = 0;
+
+        for (int i = 0; i < ints.length; i++) {
+
+            if (i == 0) {
+                ints[i] = scanner.nextInt();
+                somador+= ints[i];
+                System.out.println("N[" + i + "] = " + ints[i]);
+            } else {
+                somador+= somador;
+                ints[i] = somador;
+                System.out.println("N[" + i + "] = " + ints[i]);
+            }
+
+        }
     }
 }
