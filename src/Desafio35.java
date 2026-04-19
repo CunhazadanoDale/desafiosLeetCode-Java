cb454ce20c0d4aad23c302c6244a748fb571b3cb
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-19 09:43:58 -0300

    feat: desafio dos numerosPrimos

--- src/Desafio35.java
+++ src/Desafio35.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio35 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int quantiaDeTestes = scanner.nextInt();
+
+        for (int i = 0; i < quantiaDeTestes; i++) {
+
+            int numeroEhPrimo = scanner.nextInt();
+            boolean ehPrimoSim = true;
+
+            for (int j = 2; j < numeroEhPrimo; j++) {
+
+                if (numeroEhPrimo % j == 0) {
+                    ehPrimoSim = false;
+                    break;
+                }
+
+            }
+
+            if (ehPrimoSim) {
+                System.out.println(numeroEhPrimo + " eh primo");
+            } else {
+                System.out.println(numeroEhPrimo + " nao eh primo");
+            }
+
+        }
+
+    }
 }
