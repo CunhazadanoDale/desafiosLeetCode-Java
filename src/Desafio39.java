aa5b3408261fdeadfaf2042a25809245b4e38770
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-23 20:12:21 -0300

    feat: adição desafio de construir casa beecrowd

---
+++ src/Desafio39.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Desafio39 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (scanner.hasNext()) {
+
+            int a = scanner.nextInt();
+            if (a == 0) break;
+
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            int lado = (int) Math.sqrt((double) (a * b * 100) / c);
+
+            System.out.println(lado);
+        }
+
+    }
+}
