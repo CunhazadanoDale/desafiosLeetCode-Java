4e2e157c5d7f08925c8161a2a578e15014df6433
CunhazadaNoDale <cunha12324@gmail.com>
2026-04-23 20:12:33 -0300

    feat: desafio da arvore de natal. essa foi dificil

--- src/Desafio40.java
+++ src/Desafio40.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio40 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+
+            int n = sc.nextInt();
+
+
+            for (int i = 0; i <= n / 2; i++) {
+
+                int estrelas = 2 * i + 1;
+                int espacos = (n - estrelas) / 2;
+
+                System.out.println(" ".repeat(espacos) + "*".repeat(estrelas));
+            }
+
+
+            System.out.println(" ".repeat((n - 1) / 2) + "*");
+
+
+            System.out.println(" ".repeat((n - 3) / 2) + "***");
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
