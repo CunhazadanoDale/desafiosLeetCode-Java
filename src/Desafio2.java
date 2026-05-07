5e7fd3807e6db0b7a30c1157f9493efbcacf1d1d
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-06 23:14:38 -0300

    feat: palindrome number

--- src/Desafio2.java
+++ src/Desafio2.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Desafio2 {
+    static void main(String[] args) {
+
+        int x = -121;
+        System.out.println(isPalindrome(x));
+
+    }
+
+
+    public static boolean isPalindrome(int x) {
+
+        String valorOriginal = String.valueOf(x);
+        String sb = new StringBuilder(valorOriginal)
+                .reverse()
+                .toString();
+
+        return valorOriginal.equals(sb);
+    }
+
+
 }
